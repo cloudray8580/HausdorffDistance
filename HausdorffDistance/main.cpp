1c61f954ee0f20ab40a85e50fec49cc1b997553a
李喆 <lizhe@lizhedemacbook-pro.local>
2018-01-03 15:29:23 +0800

    Initial Commit

---
+++ HausdorffDistance/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HausdorffDistance
+//
+//  Created by 李喆 on 2018/1/3.
+//  Copyright © 2018年 李喆. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
